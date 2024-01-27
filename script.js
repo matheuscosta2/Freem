60d9198636c8494b2a11fafdea379f030f5bfb0d
Alexsander42 <asergio27@hotmail.com>
2024-01-26 23:18:09 -0300

    Modificção no header e no footer da página e alterção no script Js do carrosel

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
-// srcript responsãvel pelo carrossel
+// srcript responsável pelo carrossel
 const swiper = new Swiper('.swiper', {
     spaceBetween: 10,
     slidesPerView: 3,
     pagination: {
         el: '.swiper-pagination',
         type: 'bullets',
-    }
+        clickable: true,
+    },
+
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
 });
 
 // Tópicos
@@ -13,6 +19,6 @@ const swiper = new Swiper('.swiper', {
 let topicosLista = document.querySelector(".tópicos__lista")
 console.log(topicosLista)
 topicosLista.addEventListener("click", ()=>{
-    console.log("passou o mouse por cima")
+    console.log("clicou")
      
 })
